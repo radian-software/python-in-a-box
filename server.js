4de11dce9ce2b7428c0daf9e33c4091131ac6c5d
Radon Rosborough <radon.neon@gmail.com>
2021-05-01 20:59:29 -0700

    Fix bugs

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ ws(app);
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
 app.ws("/ws", (ws) => {
   const term = pty.spawn("python3", [], { name: "xterm-color" });
-  term.on("data", ws.send);
-  ws.on("message", term.write);
+  term.on("data", (data) => {
+    try {
+      ws.send(data);
+    } catch (err) {
+      //
+    }
+  });
+  ws.on("message", (data) => term.write(data));
 });
 
 app.listen(parseInt(process.env.PORT), "0.0.0.0");
