7c86faae17e6d295b3bc787cf53020f6889e2f75
Radon Rosborough <radon.neon@gmail.com>
2021-06-16 20:28:06 -1000

    Make robust to tampering

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+const fs = require("fs");
 const process = require("process");
 
 const express = require("express");
 const ws = require("express-ws");
 const pty = require("node-pty");
 
+const html = fs.readFileSync("index.html");
+
 const app = express();
 
 ws(app);
 
-app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(html);
+});
 app.ws("/ws", (ws) => {
   const term = pty.spawn("python3", [], { name: "xterm-color" });
   setTimeout(() => term.kill(), 3600 * 1000); // session timeout
